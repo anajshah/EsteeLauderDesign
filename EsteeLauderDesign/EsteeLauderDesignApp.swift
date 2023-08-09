baae4a4ee0c0dc410c9775b0f2c72f86d3ac1beb
Aariana <anajshah@gmail.com>
2023-08-09 16:46:10 -0700

    Initial Commit

---
+++ EsteeLauderDesign/EsteeLauderDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderDesignApp.swift
+//  EsteeLauderDesign
+//
+//  Created by Aariana Shah on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
